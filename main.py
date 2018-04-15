1de73677979eb2a83b8521cf135aaa038a9fb727
Andrzej Huk <andrzej@huk.mail.pl>
2018-04-15 20:28:14 +0200

    Changed file read to *.out files. Reading first array called Spin-Orbit Matrix

--- main.py
+++ main.py
@@ -1,28 +1,69 @@
-import sys, getopt
-from lxml import etree
+import sys, os, getopt
 import csv 
+import re
+from timeit import default_timer as timer
 
-inputfile = 'input/krb_so_t.xml' #testing only
-outputfile = 'output/out.xml' #testing only
-molProNamespace = "{http://www.molpro.net/schema/molpro-output}"
-htmlNamespace = "{http://www.w3.org/1999/xhtml}"
+def findSpinOrbitMatrix(file):
+    data = []
+    match = None
+    header = []
+    blankLineCount = 0
+    row = 0
+    i = 0
+    for line in file:
+        if not match:
+            match = re.match(" Spin-Orbit Matrix \(\S+\)", line)
+        else:
+            splittedLine = line.split()
+            if splittedLine and splittedLine[0] == 'Nr':
+                header += splittedLine
+                blankLineCount = 0
+                row = 0
+            elif len(splittedLine) > 1:
+                rowStart = 0
+                if row % 2 == 0:
+                    rowStart = 4
+                if len(data) > row:
+                    data[row] += splittedLine[rowStart:]
+                else:
+                    data.append(splittedLine)
+                    data[row] = ['']*rowStart + data[row]
+                    
+                blankLineCount = 0
+                row += 1
+            else:
+                blankLineCount += 1
+            if blankLineCount == 3: #time to stop
+                data.append([])
+                header = list(dict.fromkeys(header))    #Fix the header
+                with open('output/spinOrbitMatrixes.csv', 'a') as outputFile:
+                    writeToCSV(outputFile, header, data)                    
+                    i += 1
+                    header = []
+                    data = []
+                    match = None
+                    blankLineCount = 0
 
-def loadXMLFile(filename):
-    if not filename:
-        raise NameError
-    parser = etree.XMLParser(ns_clean=True, remove_comments = True)
-    return etree.parse(filename, parser)
+    return header, data #not needed now
 
-def writeXMLFile(filename):
-    xmlFile.write(filename)
-
-def writeToCSV(filename, header, data):
-    with open(filename+".csv", "a") as f:
-        wr = csv.writer(f)
+def writeToCSV(file, header, data):
+        wr = csv.writer(file, 'unix')
         wr.writerow(header)
         wr.writerows(data)
 
+def cleanBeforeStart(): # for testing purposes only
+    testingFiles = ['output/spinOrbitMatrixes.csv']
+    for filename in testingFiles:
+       if os.path.isfile(filename):
+            os.remove(filename)
+
 def main(argv):
+   inputFilename = 'input/krb_so_t.out' #testing only
+   outputFilename = 'output/out.csv' #testing only
+   start = timer()
+   cleanBeforeStart()
+   end = timer()
+   print("Clean time: %f" % (end-start))
    try:
       opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
    except getopt.GetoptError:
@@ -33,35 +74,15 @@ def main(argv):
          print('main.py -i <inputfile> -o <outputfile>')
          sys.exit()
       elif opt in ("-i", "--ifile"):
-         inputfile = arg
+         inputFilename = arg
       elif opt in ("-o", "--ofile"):
-         outputfile = arg
-
-def removeComments(xmlFile):
-    comments = xmlFile.xpath('//comment()')
-    for c in comments:
-        p = c.getparent()
-        p.remove(c)
+         outputFilename = arg
+   start = timer()
+   with open(inputFilename) as inputFile:
+      header, data = findSpinOrbitMatrix(inputFile)
+   end = timer()
+   print('Spin Orbit Matrix: %f' % (end-start))
 
 if __name__ == "__main__":
    main(sys.argv[1:])
-   xmlFile = loadXMLFile(inputfile)
-   #removeComments(xmlFile)
-   #writeXMLFile(outputfile)
-   root = xmlFile.getroot()
-   for child in root.getchildren():
-       if child.tag == molProNamespace+"job":
-           root = child
-           print("Found job")
-           break
-   header = []
-   data = []
-   for child in root.getchildren():
-        if child.tag == htmlNamespace+"table":
-            print("Found some tables")
-            for children in child.find(htmlNamespace+"thead"):
-                header = children.itertext()
-            for children in child.find(htmlNamespace+"tbody"):
-                data.append(elem.strip() for elem in [item for item in children.itertext() if item is not None])
-            writeToCSV("testing", header, data)
-            data.clear()
+  
